b5f469eb06237f4b89263bb795d94d14c46bd82d
Ricardo M. Vilchis <ajkaanbal@gmail.com>
2014-10-13 07:55:21 -0500

    Identify transaction response code

--- payu/payu.py
+++ payu/payu.py
@@ -21,6 +21,39 @@ PAYMENTS = 'payments'
 
 
 class PayU(object):
+    TRANSACTION_RESPONSE_CODES = {
+        'ERROR': 'Ocurrió un error general.',
+        'APPROVED': 'La transacción fue aprobada.',
+        'ANTIFRAUD_REJECTED': 'La transacción fue rechazada por el sistema anti-fraude.',
+        'PAYMENT_NETWORK_REJECTED': 'La red financiera rechazó la transacción.',
+        'ENTITY_DECLINED': 'La transacción fue declinada por el banco o por la red financiera debido a un error.',
+        'INTERNAL_PAYMENT_PROVIDER_ERROR': 'Ocurrió un error en el sistema intentando procesar el pago.',
+        'INACTIVE_PAYMENT_PROVIDER': 'El proveedor de pagos no se encontraba activo.',
+        'DIGITAL_CERTIFICATE_NOT_FOUND': 'La red financiera reportó un error en la autenticación.',
+        'INVALID_EXPIRATION_DATE_OR_SECURITY_CODE': 'El código de seguridad o la fecha de expiración estaba inválido.',
+        'INSUFFICIENT_FUNDS': 'La cuenta no tenía fondos suficientes.',
+        'CREDIT_CARD_NOT_AUTHORIZED_FOR_INTERNET_TRANSACTIONS': 'La tarjeta de crédito no estaba autorizada para transacciones por Internet.',
+        'INVALID_TRANSACTION': 'La red financiera reportó que la transacción fue inválida.',
+        'INVALID_CARD': 'La tarjeta es inválida.',
+        'EXPIRED_CARD': 'La tarjeta ya expiró.',
+        'RESTRICTED_CARD': 'La tarjeta presenta una restricción.',
+        'CONTACT_THE_ENTITY': 'Debe contactar al banco.',
+        'REPEAT_TRANSACTION': 'Se debe repetir la transacción.',
+        'ENTITY_MESSAGING_ERROR': 'La red financiera reportó un error de comunicaciones con el banco.',
+        'BANK_UNREACHABLE': 'El banco no se encontraba disponible.',
+        'EXCEEDED_AMOUNT': 'La transacción excede un monto establecido por el banco.',
+        'NOT_ACCEPTED_TRANSACTION': 'La transacción no fue aceptada por el banco por algún motivo.',
+        'ERROR_CONVERTING_TRANSACTION_AMOUNTS': 'Ocurrió un error convirtiendo los montos a la moneda de pago.',
+        'EXPIRED_TRANSACTION': 'La transacción expiró.',
+        'PENDING_TRANSACTION_REVIEW': 'La transacción fue detenida y debe ser revisada, esto puede ocurrir por filtros de seguridad.',
+        'PENDING_TRANSACTION_CONFIRMATION': 'La transacción está pendiente de ser confirmada.',
+        'PENDING_TRANSACTION_TRANSMISSION': 'La transacción está pendiente para ser trasmitida a la red financiera. Normalmente esto aplica para transacciones con medios de pago en efectivo.',
+        'PAYMENT_NETWORK_BAD_RESPONSE': 'El mensaje retornado por la red financiera es inconsistente.',
+        'PAYMENT_NETWORK_NO_CONNECTION': 'No se pudo realizar la conexión con la red financiera.',
+        'PAYMENT_NETWORK_NO_RESPONSE': 'La red financiera no respondió.',
+        'FIX_NOT_REQUIRED': 'Clínica de transacciones: Código de manejo interno.'
+    }
+
     def __init__(self,
                  api_login='',
                  api_key='',
